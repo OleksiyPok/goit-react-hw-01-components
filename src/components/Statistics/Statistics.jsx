ae41828c8d07c7314c1788922c1294c41cef515a
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2023-03-26 21:12:50 +0200

    added upload stats

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,12 +4,12 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css['stat-list']}>
         {stats.map(item => (
-          <li className="item" key={item.id}>
-            <span className="label">{item.label}</span>
-            <span className="percentage">{item.percentage}</span>
+          <li className={css.item} key={item.id}>
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}%</span>
           </li>
         ))}
       </ul>
@@ -27,3 +27,9 @@ Statistics.propTypes = {
     })
   ),
 };
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
